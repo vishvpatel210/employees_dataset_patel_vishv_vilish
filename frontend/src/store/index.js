14ce18b3a24f53f1db57fb0dfdc17948099a68a3
vishv patel <vishv.patel.cg@gmail.com>
2026-06-06 22:55:21 +0530

    feat: implement task management module

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,6 +3,7 @@ import authReducer from './slices/authSlice';
 import employeeReducer from './slices/employeeSlice';
 import departmentReducer from './slices/departmentSlice';
 import projectReducer from './slices/projectSlice';
+import taskReducer from './slices/taskSlice';
 import uiReducer from './slices/uiSlice';
 
 const store = configureStore({
@@ -11,6 +12,7 @@ const store = configureStore({
     employees: employeeReducer,
     departments: departmentReducer,
     projects: projectReducer,
+    tasks: taskReducer,
     ui: uiReducer,
   },
 });
